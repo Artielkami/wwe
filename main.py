fe520504bcca8f57fdb46d8e78d02422b0a04dfc
Artiel_kami <raynortassada@gmail.com>
2016-06-03 09:02:53 +0700

    Update lcc column at result

--- main.py
+++ main.py
@@ -53,76 +53,109 @@ def get_total_price(dep1, arr1, mid):
         return None
 
 
-def get_key(lst):
+def get_key(lst, get_len=3):
     rs = []
-    if len(lst) <= 3:
+    if len(lst) <= get_len:
         for item in lst:
             rs.append(item[1])
     else:
-        for idn in range(0, 3):
+        for idn in range(0, get_len):
             rs.append(lst[idn][1])
     return rs
 
 
 def sort_and_filter(start, end, listitem, lcc={}):
-    rs = []
-    frs = []
+    # rs = []
+    # frs = []
+    rs_lcc = []
+    rs_normal = []
     num = len(listitem)
-    if num == 2:
+    if num == 1:
         if start in lcc and listitem[0] in lcc and (listitem[0] in lcc[start] or end in lcc[listitem[0]]):
-            # if listitem[0] in lcc[start] or end in lcc[listitem[0]]:
-            return listitem
-        elif start in lcc and listitem[1] in lcc and (listitem[1] in lcc[start] or end in lcc[listitem[1]]):
-            # if listitem[1] in lcc[start] or end in lcc[listitem[1]]:
-            return list(reversed(listitem))
+            return ','.join(listitem), None
         else:
-            for item in listitem:
-                k = get_total_price(dep1=start, arr1=end, mid=item)
-                if k:
-                    tmp = [k, item]
-                    rs.append(tmp)
-            rs.sort(key=lambda x: x[0])
-            return get_key(rs)
+            return None, ','.join(listitem)
     else:
-        lcc_lst = []
         for item in listitem:
             if start in lcc and item in lcc and (item in lcc[start] or end in lcc[item]):
-                lcc_lst.append(item)
+                rs_lcc.append(item)
             else:
-                k = get_total_price(dep1=start, arr1=end, mid=item)
-                if k:
-                    tmp = [k, item]
-                    rs.append(tmp)
-        num_lcc_list = len(lcc_lst)
-        if num_lcc_list >= 3:
-            rs_lcc = []
-            for it in lcc_lst:
-                k = get_total_price(dep1=start, arr1=end, mid=it)
-                if k:
-                    tmp = [k, it]
-                    rs_lcc.append(tmp)
-            rs_lcc.sort(key=lambda x: x[0])
-            return get_key(rs_lcc)
-        elif num_lcc_list == 2:
-            rs_lcc = []
-            for it in lcc_lst:
-                k = get_total_price(dep1=start, arr1=end, mid=it)
-                if k:
-                    tmp = [k, it]
-                    rs_lcc.append(tmp)
-            rs_lcc.sort(key=lambda x: x[0])
-            frs.append(rs_lcc[0][1])
-            frs.append(rs_lcc[1][1])
-            rs.sort(key=lambda x: x[0])
-            frs.append(rs[0][1])
-            return frs
+                rs_normal.append(item)
+    num_lcc = len(rs_lcc)
+    if num_lcc >= 3 or num_lcc == num:
+        return ','.join(rs_lcc), None
+    else:
+        if num >= 3:
+            remain = 3 - num_lcc
         else:
+            remain = num - num_lcc
+
+        if remain == 1:
+            return ','.join(rs_lcc), ','.join(rs_normal)
+        else:
+            rs = []
+            for nm in rs_normal:
+                k = get_total_price(dep1=start, arr1=end, mid=nm)
+                if k:
+                    rs.append([k, nm])
             rs.sort(key=lambda x: x[0])
-            lcc_lst.append(rs[0][1])
-            lcc_lst.append(rs[1][1])
-            if num_lcc_list == 0:
-                lcc_lst.append(rs[2][1])
-            return lcc_lst
+            return ','.join(rs_lcc), ','.join(get_key(rs, remain))
+
+    # elif num == 2:
+    #     if start in lcc and listitem[0] in lcc and (listitem[0] in lcc[start] or end in lcc[listitem[0]]):
+    #         # if listitem[0] in lcc[start] or end in lcc[listitem[0]]:
+    #         return listitem
+    #     elif start in lcc and listitem[1] in lcc and (listitem[1] in lcc[start] or end in lcc[listitem[1]]):
+    #         # if listitem[1] in lcc[start] or end in lcc[listitem[1]]:
+    #         return list(reversed(listitem))
+    #     else:
+    #         for item in listitem:
+    #             k = get_total_price(dep1=start, arr1=end, mid=item)
+    #             if k:
+    #                 tmp = [k, item]
+    #                 rs.append(tmp)
+    #         rs.sort(key=lambda x: x[0])
+    #         return get_key(rs)
+    # else:
+    #     lcc_lst = []
+    #     for item in listitem:
+    #         if start in lcc and item in lcc and (item in lcc[start] or end in lcc[item]):
+    #             lcc_lst.append(item)
+    #         else:
+    #             k = get_total_price(dep1=start, arr1=end, mid=item)
+    #             if k:
+    #                 tmp = [k, item]
+    #                 rs.append(tmp)
+    #     num_lcc_list = len(lcc_lst)
+    #     if num_lcc_list >= 3:
+    #         rs_lcc = []
+    #         for it in lcc_lst:
+    #             k = get_total_price(dep1=start, arr1=end, mid=it)
+    #             if k:
+    #                 tmp = [k, it]
+    #                 rs_lcc.append(tmp)
+    #         rs_lcc.sort(key=lambda x: x[0])
+    #         return get_key(rs_lcc)
+    #     elif num_lcc_list == 2:
+    #         rs_lcc = []
+    #         for it in lcc_lst:
+    #             k = get_total_price(dep1=start, arr1=end, mid=it)
+    #             if k:
+    #                 tmp = [k, it]
+    #                 rs_lcc.append(tmp)
+    #         rs_lcc.sort(key=lambda x: x[0])
+    #         frs.append(rs_lcc[0][1])
+    #         frs.append(rs_lcc[1][1])
+    #         rs.sort(key=lambda x: x[0])
+    #         frs.append(rs[0][1])
+    #         return frs
+    #     else:
+    #         rs.sort(key=lambda x: x[0])
+    #         lcc_lst.append(rs[0][1])
+    #         lcc_lst.append(rs[1][1])
+    #         if num_lcc_list == 0:
+    #             lcc_lst.append(rs[2][1])
+    #         return lcc_lst
 
 
 def get_way(depart, desti, dictaw):
@@ -213,7 +246,7 @@ print '--------------------------------------------------'
 wb = load_workbook('final_result.xlsx')
 sheet = wb['DOM_SECTION_MST']
 start_row = 1
-end_row = 2450
+end_row = 10
 
 for row in sheet.iter_rows():
     if row[6].value == 1:
@@ -221,18 +254,18 @@ for row in sheet.iter_rows():
             _lcc[row[1].value].append(row[2].value)
         else:
             _lcc[row[1].value] = [row[2].value]
-for items in _lcc.iteritems():
-    print(items)
+# for items in _lcc.iteritems():
+#     print(items)
 for index, row in enumerate(sheet.iter_rows()):
     if start_row <= index <= end_row:
         print 'row', index, 'run ...'
-        ind = 'J' + str(index + 1)
+        inj = 'J' + str(index + 1)
+        ink = 'K' + str(index + 1)
         if row[8].value:
             tmp_lst = list(row[8].value.split(','))
-            if len(tmp_lst) > 1:
-                sheet[ind].value = ','.join(sort_and_filter(start=row[1].value, end=row[2].value, listitem=tmp_lst, lcc=_lcc))
-            else:
-                sheet[ind].value = ','.join(tmp_lst)
+            sheet[inj].value, sheet[ink] = sort_and_filter(start=row[1].value, end=row[2].value, listitem=tmp_lst, lcc=_lcc)
         print 'finish', index
+sheet['K1'] = 'LCC_RESULTS'
+sheet['J1'] = 'NORMAL_RESULTS'
 wb.save('final_result_2.xlsx')
 print 'Successful ! THIS IS SPARTA'
